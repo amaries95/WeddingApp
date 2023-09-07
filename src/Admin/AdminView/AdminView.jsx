b72241272a95fc82ce607a91052aec3d28078418
Alex Maries <alexandru.maries@centric.eu>
2023-09-07 21:59:30 +0300

    almost finished the admin section

--- src/Admin/AdminView/AdminView.jsx
+++ src/Admin/AdminView/AdminView.jsx
@@ -1,96 +1,166 @@
-import { DataGrid } from '@mui/x-data-grid';
-import { useEffect, useState } from 'react';
-import { Https, Host, AllGuests, Get, Views } from '../../Shared/UrlConstants';
+import { DataGrid } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+import { Https, Host, AllGuests, Get, Views } from "../../Shared/UrlConstants";
+import style from "./AdminView.module.css";
 
-export default function AdminView (props) {
-    const attendeesColumns  = [
-        {field: 'id', headerName: 'Id', width: 80},
-        {field: 'name', headerName: 'Nume', width: 400},
-        {field: 'numberOfGuests', headerName: '# Invitati', width: 200},
-        {field: 'isComing', headerName: 'Participa', width: 200},
-        {field: 'numberOfVeggiesMenus', headerName: '# Veggie Menus', width: 200},
-        {field: 'otherDetails', headerName: 'Observatii', width: 300},
-    ];
+export default function AdminView(props) {
+  const attendeesColumns = [
+    { field: "id", headerName: "Id", width: 80 },
+    { field: "name", headerName: "Nume", width: 400 },
+    { field: "numberOfGuests", headerName: "# Invitati", width: 200 },
+    { field: "isComing", headerName: "Participa", width: 200 },
+    { field: "numberOfVeggiesMenus", headerName: "# Veggie Menus", width: 200 },
+    { field: "otherDetails", headerName: "Observatii", width: 300 },
+  ];
 
-    const statisticsColumns = [
-        {field: 'id', headerName: 'Id', width: 80, filterable: false, sortable: false, disableColumnMenu: true},
-        {field: 'veggieMenus', headerName: 'Meniuri Veggie', width: 200, filterable: false, sortable: false, disableColumnMenu: true},
-        {field: 'normalMenus', headerName: 'Meniuri Carne', width: 200, filterable: false, sortable: false, disableColumnMenu: true},
-        {field: 'numberOfAttendees', headerName: 'Total Invitati', width: 200, filterable: false, sortable: false, disableColumnMenu: true},
-        {field: 'numberOfViews', headerName: 'Total Vizualizari', width: 200, filterable: false, sortable: false, disableColumnMenu: true},
-    ];
+  const statisticsColumns = [
+    {
+      field: "id",
+      headerName: "Id",
+      width: 80,
+      filterable: false,
+      sortable: false,
+      disableColumnMenu: true,
+    },
+    {
+      field: "veggieMenus",
+      headerName: "Meniuri Veggie",
+      width: 200,
+      filterable: false,
+      sortable: false,
+      disableColumnMenu: true,
+    },
+    {
+      field: "normalMenus",
+      headerName: "Meniuri Carne",
+      width: 200,
+      filterable: false,
+      sortable: false,
+      disableColumnMenu: true,
+    },
+    {
+      field: "numberOfAttendees",
+      headerName: "Total Invitati",
+      width: 200,
+      filterable: false,
+      sortable: false,
+      disableColumnMenu: true,
+    },
+    {
+      field: "numberOfViews",
+      headerName: "Total Vizualizari",
+      width: 200,
+      filterable: false,
+      sortable: false,
+      disableColumnMenu: true,
+    },
+  ];
 
-    var [attendeesRows, setAttendeesRows] = useState(null);
-    var [statisticsRows, setStatisticsRows] = useState(null);
+  var [attendeesRows, setAttendeesRows] = useState(null);
+  var [statisticsRows, setStatisticsRows] = useState(null);
 
-    useEffect(() => {
-        getAllGuests();
-        getAllViews();
-    }, []);
+  useEffect(() => {
+    getAllData();
+  }, []);
 
-    const getAllGuests = async () => {
-        var response = await fetch(Https + Host + AllGuests,
-            {
-                method: Get,
-                headers: {
-                'Content-Type': 'application/json',
-                'Accept': '*/*',
-                'Host': Host,
-                'Authorization': 'Bearer ' + localStorage.getItem('token')
-                }
-            });
-            
-        setAttendeesRows(await response.json());
-        console.log("getGuests");
-    }
+  const getAllData = () => {
+    var statistics = fetch(Https + Host + AllGuests, {
+      method: Get,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+        Host: Host,
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Get all guest request was not ok");
+        }
 
-    const getAllViews = async () => {
-        var response = await fetch(Https + Host + Views,
-            {
-                method: Get,
-                headers: {
-                'Content-Type': 'application/json',
-                'Accept': '*/*',
-                'Host': Host,
-                'Authorization': 'Bearer ' + localStorage.getItem('token')
-                }
-            });
-            var viewsResponse = await response.json();
-        
+        return response.json();
+      })
+      .then((response) => {
+        setAttendeesRows(response);
+
+        var statistics = calculateStatistics(response);
+
+        fetch(Https + Host + Views, {
+          method: Get,
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "*/*",
+            Host: Host,
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Get all views request was not ok");
+            }
+
+            return response.json();
+          })
+          .then((response) => {
             setStatisticsRows([
-                {
-                    id: 1, 
-                    veggieMenus: 200, 
-                    normalMenus: 50, 
-                    numberOfAttendees: 140, 
-                    numberOfViews: viewsResponse.numberOfViews}
+              {
+                id: 1,
+                veggieMenus: statistics.veggieMenus,
+                normalMenus: statistics.normalMenus,
+                numberOfAttendees: statistics.numberOfAttendees,
+                numberOfViews: response.numberOfViews,
+              },
             ]);
-    }
-    
-    const onLogoutEvent = (event) => {
-        props.logoutProp();
-    }
+          });
+      });
 
-    return (
-        <>
-            <h2>Admin</h2>
-            <button onClick={onLogoutEvent}>Log out</button>
-            <DataGrid
-                columns={statisticsColumns}
-                rows={statisticsRows !== null ? statisticsRows : []}
-                hideFooter
-                disableColumnSelector
-            ></DataGrid>
-            <DataGrid
-                columns={attendeesColumns}
-                rows={attendeesRows !== null ? attendeesRows : []}
-                initialState={{
-                    pagination: {
-                        paginationModel: {page: 0, pageSize: 30}
-                    },
-                }}
-                pageSizeOptions={[5,10]}
-            ></DataGrid>
-        </>
-    );
+    console.log("getGuests");
+  };
+
+  const calculateStatistics = (atendees) => {
+    var statistics = {
+      veggieMenus: 0,
+      normalMenus: 0,
+      numberOfAttendees: 0,
+    };
+
+    atendees.forEach((element) => {
+      if (element.isComing) {
+        statistics.numberOfAttendees += element.numberOfGuests;
+        statistics.veggieMenus += element.numberOfVeggiesMenus;
+      }
+    });
+
+    statistics.normalMenus =
+      statistics.numberOfAttendees - statistics.veggieMenus;
+
+    return statistics;
+  };
+
+  const onLogoutEvent = (event) => {
+    props.logoutProp();
+  };
+
+  return (
+    <>
+      <h2>Admin</h2>
+      <button onClick={onLogoutEvent}>Log out</button>
+      <DataGrid
+        columns={statisticsColumns}
+        rows={statisticsRows !== null ? statisticsRows : []}
+        hideFooter
+        disableColumnSelector
+      ></DataGrid>
+      <DataGrid
+        columns={attendeesColumns}
+        rows={attendeesRows !== null ? attendeesRows : []}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 30 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+      ></DataGrid>
+    </>
+  );
 }
